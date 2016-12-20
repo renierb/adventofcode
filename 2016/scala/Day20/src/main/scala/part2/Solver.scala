0a3777d858ad1d9cd240ffe47fea33baf8d8d5ee
Renier Botes <rlbotes@gmail.com>
2016-12-20 18:31:11 +0200

    day20 part 1 + 2

--- 2016/scala/Day20/src/main/scala/part2/Solver.scala
+++ 2016/scala/Day20/src/main/scala/part2/Solver.scala
@@ -9,19 +9,27 @@ class Solver(input: SortedSet[(Long, Long)]) {
   val max: Long = 4294967295L
 
   def solve: Long = {
-    explore(input).getOrElse(-1L)
+    explore(input)
   }
 
   @tailrec
-  private def explore(remainder: SortedSet[(Long, Long)]): Option[Long] = {
+  private def explore(remainder: SortedSet[(Long, Long)], acc: Long = 0): Long = {
     if (remainder.isEmpty)
-      None
+      acc
     else {
       val ip = remainder.head._2 + 1
       if (input.exists(p => p._1 <= ip && p._2 >= ip))
-        explore(remainder.tail)
-      else
-        Some(ip)
+        explore(remainder.tail, acc)
+      else {
+        val count = stream(ip, max).takeWhile(n => !input.exists(p => p._1 <= n && p._2 >= n)).length
+        explore(remainder.tail, acc + count)
+      }
     }
   }
+
+  def stream(from: Long, to: Long): Stream[Long] =
+    if (from > to)
+      Stream.empty
+    else
+      from #:: stream(from + 1, to)
 }
